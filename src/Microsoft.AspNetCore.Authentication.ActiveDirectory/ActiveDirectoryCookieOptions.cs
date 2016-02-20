2b9afadba2431deee612a597060e49a073995490
Radi A <radi.a@live.com.au>
2016-02-20 11:59:33 +0200

    Added the ability to instantiate with custom Cookie options

--- src/Microsoft.AspNetCore.Authentication.ActiveDirectory/ActiveDirectoryCookieOptions.cs
+++ src/Microsoft.AspNetCore.Authentication.ActiveDirectory/ActiveDirectoryCookieOptions.cs
@@ -12,27 +12,39 @@ namespace Microsoft.AspNetCore.Authentication.ActiveDirectory
     /// </summary>
     public class ActiveDirectoryCookieOptions
     {
-
-            public ActiveDirectoryCookieOptions()
+        /// <summary>
+        /// Default constructor for AD Cookie Options. Internally instantiates CookieAuthenticationOptions with default values.
+        /// </summary>
+        public ActiveDirectoryCookieOptions()
+        {
+            // Configure all of the cookie middlewares
+            ApplicationCookie = new CookieAuthenticationOptions
             {
-                // Configure all of the cookie middlewares
-                ApplicationCookie = new CookieAuthenticationOptions
-                {
-                    AuthenticationScheme = ApplicationCookieAuthenticationScheme,
-                    AutomaticAuthenticate = true,
-                    AutomaticChallenge = true,
-                    ReturnUrlParameter = "ReturnUrl",
-                    LoginPath = new PathString("/windowsauthentication/ntlm"),
-                };
-            }
+                AuthenticationScheme = ApplicationCookieAuthenticationScheme,
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                ReturnUrlParameter = "ReturnUrl",
+                LoginPath = new PathString("/windowsauthentication/ntlm"),
+            };
+        }
 
-            public CookieAuthenticationOptions ApplicationCookie { get; set; }
+        /// <summary>
+        /// Constructor accepting CookieAuthenticationOptions
+        /// </summary>
+        /// <param name="cookieOptions"></param>
+        public ActiveDirectoryCookieOptions(CookieAuthenticationOptions cookieOptions)
+        {
+            // Configure all of the cookie middlewares
+            ApplicationCookie = cookieOptions;
+        }
 
-            /// <summary>
-            /// Gets or sets the scheme used to identify application authentication cookies.
-            /// </summary>
-            /// <value>The scheme used to identify application authentication cookies.</value>
-            public string ApplicationCookieAuthenticationScheme { get; set; } = typeof(ActiveDirectoryCookieOptions).Namespace + ".Application";
+        public CookieAuthenticationOptions ApplicationCookie { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scheme used to identify application authentication cookies.
+        /// </summary>
+        /// <value>The scheme used to identify application authentication cookies.</value>
+        public string ApplicationCookieAuthenticationScheme { get; set; } = typeof(ActiveDirectoryCookieOptions).Namespace + ".Application";
 
     }
 }

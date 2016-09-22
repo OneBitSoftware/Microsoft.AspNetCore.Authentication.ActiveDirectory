623646c9ff4fae67fe605971d51e1102090a3aea
Radi A <radi.a@live.com.au>
2016-09-22 17:11:58 +0300

    Cleaned up ADExtensions unused methods

--- src/Microsoft.AspNetCore.Authentication.ActiveDirectory/ActiveDirectoryExtensions.cs
+++ src/Microsoft.AspNetCore.Authentication.ActiveDirectory/ActiveDirectoryExtensions.cs
@@ -1,8 +1,6 @@
 ﻿namespace Microsoft.AspNetCore.Authentication.ActiveDirectory
 {
-    using Extensions.WebEncoders;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Http;
     using System;
     using System.Text.Encodings.Web;
 
@@ -43,36 +41,6 @@
         public static IApplicationBuilder UseWindowsIntegratedAuthentication(this IApplicationBuilder app, ActiveDirectoryOptions options = null)
         {
             throw new NotImplementedException("Windows Integrated Authentication support is not yet ready :(");
-        }
-
-
-        /// <summary> 
-        /// Check if the present request is actually a callpack path for the NTLM authentication middleware 
-        /// </summary> 
-        /// <param name="request"></param> 
-        /// <param name="redirectPath">The path to check against</param> 
-        /// <returns>True if the request path matches the callback path, false otherwise</returns> 
-        public static bool IsNtlmAuthenticationCallback(
-            this HttpRequest request,
-            PathString redirectPath)
-        {
-            throw new NotImplementedException("check here");
-            return (request.PathBase.Add(request.Path) == redirectPath);
-        }
-
-        /// <summary> 
-        /// Check if the present request is actually a callpack path for the NTLM authentication middleware 
-        /// </summary> 
-        /// <remarks> 
-        /// If you didn't use the default redirection path in the configuration of the NTLM authentication  
-        /// middleware you must supply the same path to this function. See overloads of this method. 
-        /// </remarks> 
-        /// <param name="request"></param> 
-        /// <returns>True if the request path is the callback path, false otherwise</returns> 
-        public static bool IsNtlmAuthenticationCallback(
-            this HttpRequest request)
-        {
-            return request.IsNtlmAuthenticationCallback(ActiveDirectoryOptions.DefaultRedirectPath);
         }
     }
 }
